eb907e335e53eff7f4dc5db75ae912679199c786
sunwei <sw20002000@163.com>
2024-09-21 16:08:56 +0800

    ignore

---
+++ lib/tlpi_hdr.h
@@ -0,0 +1,19 @@
+#ifndef TLPI_HDR_H
+#define TLPI_HDR_H
+// 系统调用所需要的头文件
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// #include "get_num.h"
+#include "error_functions.h"
+
+typedef enum { FALSE, TRUE } Boolean;
+#define min(m, n) ((m) < (n) ? (m) : (n))
+#define max(m, n) ((m) > (n) ? (m) : (n))
+
+#endif
